856036da8ba5dcd82e58fbc88d9ab960a138189f
AdiSploitsSplash <79107363+AdiSploitsSplash@users.noreply.github.com>
2021-06-14 23:35:37 +0300

    Update and rename .gitignore to .lua

---
+++ .lua
@@ -0,0 +1,11 @@
+game:GetService("Lighting").Brightness = 0
+	game:GetService("Lighting").TimeOfDay = 0
+	spawn(function()
+		wait(1)
+		while _G['property_nightmode'] == true do
+			game:GetService("Lighting").Brightness = 0
+			game:GetService("Lighting").TimeOfDay = 0
+			wait(1)
+		end
+	end)
+end)
